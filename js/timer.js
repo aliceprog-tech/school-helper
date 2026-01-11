48f671eddc768c1fdeb0b68d5242c3ad92af1cda
aliceprog-tech <dtt3631@gmail.com>
2026-01-11 15:15:57 +0300

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,15 +1,32 @@
 let seconds = 1500;
-let interval;
+let interval = null;
+
+const timeEl = document.getElementById("time");
+
+function updateTime() {
+  const min = Math.floor(seconds / 60);
+  const sec = seconds % 60;
+  timeEl.textContent = ${min}:${sec < 10 ? "0" : ""}${sec};
+}
 
 function start() {
   if (interval) return;
 
   interval = setInterval(() => {
     seconds--;
-    const min = Math.floor(seconds / 60);
-    const sec = seconds % 60;
-    time.textContent = `${min}:${sec < 10 ? "0" : ""}${sec}`;
+    updateTime();
 
-    if (seconds <= 0) clearInterval(interval);
+    if (seconds <= 0) {
+      clearInterval(interval);
+      interval = null;
+      alert("Время вышло!");
+    }
   }, 1000);
+}
+
+function reset() {
+  clearInterval(interval);
+  interval = null;
+  seconds = 1500;
+  updateTime();
 }
