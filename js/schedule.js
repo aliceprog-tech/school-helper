c7d9dc0e8fe22f6ef96de25dcc6c62448d16d9dc
aliceprog-tech <dtt3631@gmail.com>
2026-01-11 16:01:27 +0300

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -1,1 +1,13 @@
+const data = {
+  mon: ["Математика", "Русский", "История"],
+  tue: ["Английский", "Физика", "Информатика"]
+};
 
+function showDay(day) {
+  lessons.innerHTML = "";
+  data[day].forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    lessons.appendChild(li);
+  });
+}
