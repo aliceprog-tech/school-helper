8b2d6637d6922d80cb72ec289422fd04d13f405f
aliceprog-tech <dtt3631@gmail.com>
2026-01-11 14:32:53 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,11 @@
+function register() {
+  const name = document.getElementById("name").value;
+  const userClass = document.getElementById("class").value;
 
+  localStorage.setItem(
+    "user",
+    JSON.stringify({ name, userClass })
+  );
+
+  alert("Данные сохранены!");
+}
