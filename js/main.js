def24da1f3ff452d18b64324d309eddc7dcc60b8
aliceprog-tech <dtt3631@gmail.com>
2026-01-11 14:28:10 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+const themeBtn = document.getElementById("themeBtn");
 
+menuBtn.onclick = () => {
+  sidebar.classList.toggle("active");
+};
+
+themeBtn.onclick = () => {
+  document.body.classList.toggle("dark");
+
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
+};
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
